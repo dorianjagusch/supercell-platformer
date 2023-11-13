5da77b7bdda69ff5a3829de4c94f2aea5766867f
Dorian home <dorian.jagusch@gmail.com>
2023-11-13 07:08:59 +0200

    initialise repo

---
+++ MYREADME.md
@@ -0,0 +1,2 @@
+# supercell-platformer
+A coding challenge exclusively for Hive Helsinki 2023 set by Supercell
---
+++ src/Player.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Rectangle.h"
+
+
+struct InputData;
+
+class Game;
+
+class Player : public Rectangle
+{
+public:
+    Player(Game* pGame);
+    virtual ~Player() {}
+    
+    bool initialise(){};
+    void move(InputData inputData, float deltaTime);
+    void updatePhysics(float deltaTime);
+    void update(float deltaTime);
+
+    int getCoins() const { return m_coins; };
+
+    bool isDead() const { return m_isDead; }
+    void setIsDead(bool isDead) { m_isDead = isDead; }
+
+private:
+    bool    m_isGrounded;
+    bool    m_isDead = false;
+    float   m_jumpTimer = 0.0f;
+    Game*   m_pGame;
+    int     m_coins = 0;
+};
